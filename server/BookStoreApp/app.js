0823dbeb82061ba0e6e7f4ac1b5c7bc4dfd39c62
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-07-16 17:33:26 -0400

    final

--- server/BookStoreApp/app.js
+++ server/BookStoreApp/app.js
@@ -16,13 +16,13 @@ app.use(cors());
 app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
 app.use(logger("dev"));
-app.use(express.json());
-=======
-app.use(logger("dev"));
->>>>>>> mintes
+// app.use(express.json());
+// =======
+// app.use(logger("dev"));
+// >>>>>>> mintes
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
